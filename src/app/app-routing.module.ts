b5024a1f055385d482a03f53f001321cc35ab6ab
plyschik <palyschik@gmail.com>
2019-01-31 16:31:31 +0100

    Navbar with routing added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { BoardPageComponent } from './board-page/board-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainPageComponent, pathMatch: 'full' },
+  { path: 'board', component: BoardPageComponent },
+  { path: 'about', component: AboutPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { BoardPageComponent } from './board-page/board-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    AboutPageComponent,
+    BoardPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
